887fbdb3aa69c04f1817de73ffb6a7c7712ec674
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-06-12 19:55:34 +0800

    Next Link has been implement in the top character page

--- components/CollectionTopCharacter.tsx
+++ components/CollectionTopCharacter.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Image from "next/image";
 import { useInfiniteQuery } from "react-query";
 import { FaHeart } from "react-icons/fa";
+import Link from "next/link";
 
 const fetchTopCharacter: any = ({ pageParam = 1 }) => {
   return axios.get(`https://api.jikan.moe/v4/top/characters?page=${pageParam}`);
@@ -42,32 +43,34 @@ const CollectionTopCharacter = () => {
           {dataAnime?.pages?.map((page: any, i: number) => {
             return page?.data?.data.map((data: any, i: number) => {
               return (
-                <div
-                  key={i}
-                  className="flex flex-col bg-[#1C2024] hover:bg-[#212529] rounded-lg overflow-hidden cursor-pointer">
-                  <div className="flex relative">
-                    <Image
-                      src={data.images.jpg.image_url}
-                      alt="Top character image"
-                      width={160}
-                      height={280}
-                      priority
-                      className="w-full"
-                    />
+                <Link href={`/character/${data.mal_id}`}>
+                  <div
+                    key={i}
+                    className="flex flex-col bg-[#1C2024] hover:bg-[#212529] rounded-lg overflow-hidden h-full cursor-pointer">
+                    <div className="flex relative">
+                      <Image
+                        src={data.images.jpg.image_url}
+                        alt="Top character image"
+                        width={160}
+                        height={280}
+                        priority
+                        className="w-full"
+                      />
 
-                    {data.favorites != null && (
-                      <p className="flex items-center gap-1 absolute bottom-1 left-1 bg-[#212529] py-0.5 px-2 rounded-xl text-xs">
-                        <FaHeart className="text-[#d00000]" size={12} />{" "}
-                        {data.favorites.toLocaleString("en-US")}
-                      </p>
-                    )}
+                      {data.favorites != null && (
+                        <p className="flex items-center gap-1 absolute bottom-1 left-1 bg-[#212529] py-0.5 px-2 rounded-xl text-xs">
+                          <FaHeart className="text-[#d00000]" size={12} />{" "}
+                          {data.favorites.toLocaleString("en-US")}
+                        </p>
+                      )}
+                    </div>
+                    <div className="p-2">
+                      <h3 className="text-white font-semibold text-base">
+                        {data.name != null && data.name}
+                      </h3>
+                    </div>
                   </div>
-                  <div className="p-2">
-                    <h3 className="text-white font-semibold text-base">
-                      {data.name != null && data.name}
-                    </h3>
-                  </div>
-                </div>
+                </Link>
               );
             });
           })}
