9808efb9de17049289ce2ab6d1a2b9feb3aef862
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-06-15 20:01:09 +0800

    change the width of combobox

--- components/CollectionComboboxDemo.tsx
+++ components/CollectionComboboxDemo.tsx
@@ -44,7 +44,7 @@ export const ComboboxDemo: React.FC<ComboboxDemoProps> = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
+      <PopoverContent className="p-0">
         <Command className="w-full ">
           <CommandInput placeholder="Search genre..." />
           <CommandEmpty>No framework found.</CommandEmpty>
